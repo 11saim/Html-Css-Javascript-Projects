ff4e6c090365f5c27b2c99684fe0d80dc5394451
Saim Shabbir <shabbirsaim333@gmail.com>
2025-10-13 22:29:12 +0500

    Search Functionality Added

--- Rest Countries Api/index.js
+++ Rest Countries Api/index.js
@@ -5,6 +5,7 @@ import { loadPagination } from "../Pagination/script.js";
 const cardArea = document.querySelector(".card-area")
 const card = document.querySelector(".card")
 const filter = document.querySelector(".filter .selected")
+const input = document.querySelector("input")
 let data = null;
 
 
@@ -27,18 +28,23 @@ filter.addEventListener("click", () => {
     document.querySelector(".options").classList.toggle("opacity-100");
 })
 
+// Funcion To Create Card
+function createCard(i) {
+    let newCard = card.cloneNode(true);
+    newCard.classList.remove("hidden")
+    newCard.querySelector("img").src = data[i]['flags']['png']
+    newCard.querySelector("h1").innerText = data[i]['name'];
+    newCard.querySelectorAll('p')[0].querySelector('span').innerText = data[i]['population'].toLocaleString()
+    newCard.querySelectorAll('p')[1].querySelector('span').innerText = data[i]['region']
+    newCard.querySelectorAll('p')[2].querySelector('span').innerText = data[i]['capital']
+    cardArea.appendChild(newCard);
+}
+
 // Function To Display Cards
 function displayCards(start, end) {
     cardArea.innerHTML = "";
     for (let i = start; i < end; i++) {
-        let newCard = card.cloneNode(true);
-        newCard.classList.remove("hidden")
-        newCard.querySelector("img").src = data[i]['flags']['png']
-        newCard.querySelector("h1").innerText = data[i]['name'];
-        newCard.querySelectorAll('p')[0].querySelector('span').innerText = data[i]['population'].toLocaleString()
-        newCard.querySelectorAll('p')[1].querySelector('span').innerText = data[i]['region']
-        newCard.querySelectorAll('p')[2].querySelector('span').innerText = data[i]['capital']
-        cardArea.appendChild(newCard);
+        createCard(i);
     }
 }
 
@@ -58,7 +64,7 @@ loadPagination(16);
 import { currPage } from "../Pagination/script.js";
 
 // Pagination Area Variable
-const Pagination = document.querySelector(".pagination")
+const Pagination = document.querySelector(".pagination div")
 
 // Initializing Observer For Pagination 
 const observer = new MutationObserver((mutations) => {
@@ -69,3 +75,32 @@ const observer = new MutationObserver((mutations) => {
 // Applying Observer On Pagination So Page Change Can Be Detected
 observer.observe(Pagination, { childList: true, subtree: true, characterData: true, attributes: true });
 
+// Debouncing Function
+function debounce(func, delay) {
+    let timer;
+    return function (...args) {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            func.apply(this, args);
+        }, delay);
+    };
+}
+
+// Function For Search Input
+function inputHandler(e) {
+    if (e.target.value.trim() == "") {
+        Pagination.parentElement.style.display = "flex";
+        calcCardRange(currPage);
+    } else {
+        Pagination.parentElement.style.display = "none";
+        cardArea.innerHTML = "";
+        for (let i = 0; i < data.length; i++) {
+            if (data[i].name.toLowerCase().includes(e.target.value.toLowerCase())) {
+                createCard(i);
+            }
+        }
+    }
+}
+
+// Event Listener For Search Input
+input.addEventListener("input", debounce(inputHandler, 400))
